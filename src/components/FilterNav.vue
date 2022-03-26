da0b16cbd26211596aaa5253a9aab9988ff08709
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-26 22:15:26 +0800

    style the filterNav

--- src/components/FilterNav.vue
+++ src/components/FilterNav.vue
@@ -17,4 +17,19 @@ export default {
 </script>
 
 <style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
 </style>
