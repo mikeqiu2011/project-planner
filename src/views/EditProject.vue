ba7722f5f3282e048ebac693ec3c5e087be4bf1f
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-26 20:31:49 +0800

    my solution for edit project

---
+++ src/views/EditProject.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="editproject" v-if="title">
+    <form @submit.prevent="editProject">
+      <label>Title</label>
+      <input type="text" v-model="title" required />
+      <label>Details</label>
+      <textarea v-model="detail" required />
+      <button>Submit</button>
+    </form>
+  </div>
+  <div v-else>
+    <p>Loading project info</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      title: "",
+      detail: "",
+      //   project: null,
+      uri: "http://localhost:3000/projects/" + this.id,
+    };
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        // this.project = data;
+        this.title = data.title;
+        this.detail = data.detail;
+        console.log(this.title);
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    editProject() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: this.title,
+          detail: this.detail,
+        }),
+      })
+        .then(this.$router.push("/"))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
