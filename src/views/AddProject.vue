ba993ee53fbcabd696de8fd1d40dd538476f54c5
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-26 19:14:03 +0800

    now can add project, submit.prevent modifier

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
-    <p>add a new project</p>
+    <form @submit.prevent="addProject">
+      <label>Title</label>
+      <input type="text" v-model="title" required />
+      <label>Details</label>
+      <textarea v-model="detail" required />
+      <button>Add</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      uri: "http://localhost:3000/projects",
+      title: "",
+      detail: "",
+    };
+  },
+  methods: {
+    addProject() {
+      fetch(this.uri, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          title: this.title,
+          detail: this.detail,
+          complete: false,
+        }),
+      }).catch((err) => console.log(err));
+    },
+  },
+};
 </script>
 
 <style>
